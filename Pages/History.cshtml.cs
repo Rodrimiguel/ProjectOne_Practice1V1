9c85f615f7a6b78354378ddc49fc442805e0cacf
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-04-28 01:47:29 -0300

    initial Commit

---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TESTPROJECT_PRACTICE1V2
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
